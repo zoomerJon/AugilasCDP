9f4532d188ec1703068b10361dd3a05fa80e6716
Jonathan Ovalle <jovalle0103@gmail.com>
2022-10-11 11:37:34 -0700

    removed gsap

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 // gsap.from(".hero", { duration: 2, y: "-100%", ease: "bounce" });
-gsap.from(".top-section", { duration: 2, x: "-100%", ease: "bounce" });
+// gsap.from(".top-section", { duration: 2, x: "-100%", ease: "bounce" });
